0fce84df5eeb1da4f7094357a49982a231cc4055
jYoung15628 <jylee9802@naver.com>
2025-01-06 17:46:50 +0900

    2025-01-06

---
+++ 13-event/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SynthesticEvent from './SynthesticEvent';
+
+function App() {
+  return (
+    <div className="App">
+      <SynthesticEvent></SynthesticEvent>
+    </div>
+  );
+}
+
+export default App;
